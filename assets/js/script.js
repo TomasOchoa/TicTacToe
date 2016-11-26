650ac18e488d8ecead26bc6109d68c77fb77413f
TomasOchoa <ochoa.tom91@gmail.com>
2016-11-26 15:05:41 -0500

    Include custom images for 'X' & 'O'. Add click listener for box. Setup basic styling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
 /**
  * Created by Tom's Desktop on 11/25/2016.
  */
+
+"use strict";
+var cross  = "url('assets/images/cross.png')";          //'X'
+var circle = "url('assets/images/circle.png')";         //'O'
+var turn = 1;
+
+$(function () {
+    $('td').click(function () {
+        var clickedBox = $(this);
+        if(turn === 1){
+            clickedBox.css('background-image',cross).fadeIn("slow");
+            return turn = 2;
+        }
+        if(turn === 2){
+            clickedBox.css('background-image',circle).fadeIn("slow");
+            return turn = 1;
+        }
+
+    });
+}(jQuery));
+
+
+function changeState(turn,clicked) {
+
+
+
+
+}
