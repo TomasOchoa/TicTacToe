35cf8c84daffd9266d108d0daeaa187547bda2ea
TomasOchoa <ochoa.tom91@gmail.com>
2016-11-29 13:57:22 -0500

    Finish Tie Logic. Bug fix that allowed the display to change after a game is over.

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,43 +27,51 @@ $(function () {
 
         //Box Listener
         tableBoxes.click(function () {
-            var clickedBox = $(this);
             //Variable that holds the box clicked
-            if(clickedBox.hasClass('clicked') || clickedBox.hasClass('locked')){
+            var clickedBox = $(this);
+
+            //If marked or locked, prevent click event
+            if(clickedBox.hasClass('locked') || (ttt.checkWinner())){
                 return;
+            }
+            //If not valid move and there are less than 9 turns played, display error message
+            else if(clickedBox.hasClass('clicked') && ttt.turnsPlayed < 9){
+                ttt.changeMessageDisplay('Pick a different box!');
+                ttt.displayMessage.addClass('error');
+                /*
+                 Highlight all available moves
+                 border: red solid;
+                 border: solid red 5px;
+                 height: 100%;
+                 width: 100%;
+                 padding: initial;
+                 margin: inherit;
+                 */
             }
             else {
                 //If pieces !set yet, show error
                 if(ttt.player1.piece === ''){
                     ttt.displayMessage.addClass('error');
                 }
-                //If not valid move and there are less than 9 turns played
-                else if((!ttt.verifyMove(clickedBox)) && (ttt.turnsPlayed <= 9)){
-                    //Display error message
-                    ttt.changeMessageDisplay('Pick a different box!');
-                    ttt.displayMessage.addClass('error');
-                    /*
-                     Highlight all available moves
-                     border: red solid;
-                     border: solid red 5px;
-                     height: 100%;
-                     width: 100%;
-                     padding: initial;
-                     margin: inherit;
-                     */
-                }
                 //If valid move and still valid turn
-                else if(ttt.verifyMove(clickedBox) && ttt.turnsPlayed <= 9){
+                else if (ttt.verifyMove(clickedBox) && ttt.turnsPlayed <= 9){
                     ttt.displayMessage.removeClass('error');
                     ttt.changeMessageDisplay('Make your move!')
                     ttt.setBox(clickedBox);
-                    if(ttt.checkWinner()){
+
+                    //If tied
+                    if (ttt.turnsPlayed === 9 && !ttt.checkWinner()) {
+                        ttt.showTie();
+                        ttt.reset(ttt);
+                    }
+                    //If Winner
+                    else if (ttt.checkWinner()){
                         ttt.showWinner();
                         ttt.reset(ttt);
                     }
-                    else{
+                    //Vaild move
+                    else
                         ttt.setNextTurn();
-                    }
                 }
             }
         });
